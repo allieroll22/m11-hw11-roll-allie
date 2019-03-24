020297691d8e8fa0b0df0907e32e523409dc68cd
“Allie <allieroll22@ufl.edu>
2019-03-24 15:57:35 -0400

    Add functionality and event listener

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
 //Global Varibales
-
+const myBoxes = document.getElementsByClassName('block');
 
 //The Functionality
+function loopThrough() {
+
+};
 
 
 //The Event Listener
+for (let i = 0; i < myBoxes.length; i++) {
+  myBoxes[i].addEventListener('click', loopThrough);
+};
